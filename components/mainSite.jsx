6f67016d1fe0f8298352a7cb9ff103b3d9c33d40
makla12 <144844324+makla12@users.noreply.github.com>
2024-10-20 14:23:45 +0200

    add footer component

--- components/mainSite.jsx
+++ components/mainSite.jsx
@@ -1,11 +1,14 @@
 import { Nav } from "./nav";
+import { Footer } from "./footer";
 
 export function MainSite(){
     
     return(
         <>
-            <Nav />
-            <div></div>
+            <div className="min-h-screen grid">
+                <Nav />
+                <Footer />
+            </div>
         </>
     );
 }
--- components/nav.jsx
+++ components/nav.jsx
@@ -4,7 +4,12 @@ import logo from "@/public/logo.png";
 export function Nav(){
     
     return(
-        <div className="w-screen min-w-fit h-24 bg-[#252a54] px-40 text-white flex items-center text-2xl gap-36 shrink-0">
+        <div className="
+            w-screen min-w-fit h-24 px-40
+            bg-[#252a54]
+            text-white text-2xl
+            flex items-center gap-36 shrink-0"
+        >
             <a className="w-24 h-full flex items-center" href="/">
                 <Image src={logo} alt="Logo"/>
             </a>
